abcc41f7178c58de505b3f9281cb262a2cfaf2b8
Gabriel-Strand <st8048ru@go.minnstate.edu>
2025-04-26 21:26:31 -0500

    Added Correct Functionality

--- main.bat
+++ main.bat
@@ -1,3 +1,5 @@
-python main_python.py
+curl -o main.py https://raw.githubusercontent.com/Gabriel-Strand/YouTubeAudioGrabber/refs/heads/main/main_python.py
+python main.py
+del main.py
 rmdir /s /q "__pycache__"
 pause
